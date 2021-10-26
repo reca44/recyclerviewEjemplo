329327b4d5e2f25a78a497582b9ec7033dd12e71
vinlopant <antoniovilopez@gmail.com>
2021-10-26 09:10:59 +0200

    primera version

---
+++ app/src/main/java/com/murallaromana/proyectorecyclesview/activities/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.murallaromana.proyectorecyclesview.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.murallaromana.proyectorecyclesview.R
+import com.murallaromana.proyectorecyclesview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       // binding= ActivityMainBinding.bind()
+    }
+}
